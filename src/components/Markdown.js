89e54ec1642f4b00b7bbc2995ddf376f2efaa5a5
Benjamin Hetherington <b.w.hetherington@gmail.com>
2018-12-20 19:47:40 -0500

    Add support for jumping to anchors

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -10,6 +10,20 @@ function makeHeadingRenderer() {
   }
 }
 
+function LinkRenderer(props) {
+  const { href } = props;
+  const hash = href.split(/#/)[1];
+  const encodedURI = encodeURIComponent(href);
+  const { origin, pathname } = window.location;
+  const newHref = hash !== undefined
+    ? `${origin}${pathname}?url=${encodedURI}#${hash}`
+    : `${origin}${pathname}?url=${encodedURI}`;
+  const onClick = () => {
+    window.location.href = newHref;
+  };
+  return <span className="link" onClick={onClick}>{props.children}</span>
+}
+
 export default class Markdown extends React.Component {
   state = {
     markdown: null,
@@ -19,12 +33,19 @@ export default class Markdown extends React.Component {
   async componentDidMount() {
     try {
       const { url } = this.props;
-      const response = await fetch(url);
+      const [href, anchor] = url.split(/#/);
+      const response = await fetch(href);
       const markdown = await response.text();
       this.setState({
         ...this.state,
         markdown
       });
+      if (anchor !== undefined) {
+        const node = document.getElementById(anchor);
+        if (node !== null && node !== undefined) {
+          node.scrollIntoView(true);
+        }
+      }
     } catch (err) {
       console.log(err);
       this.setState({
@@ -41,7 +62,18 @@ export default class Markdown extends React.Component {
       return <div className="error">Error loading resource: <span className="code">{url}</span></div>;
     } else if (markdown !== null && markdown !== undefined) {
       const HeadingRenderer = makeHeadingRenderer();
-      return <div className="page"><ReactMarkdown source={markdown} renderers={{heading: HeadingRenderer}} /></div>;
+      const renderers = {
+        heading: HeadingRenderer,
+        link: LinkRenderer
+      };
+      return (
+        <div className="page">
+          <ReactMarkdown 
+            source={markdown} 
+            renderers={renderers} 
+          />
+        </div>
+      );
     } else {
       return <div />;
     }
